ef43299cb4a7320d1f27e6b5c7c9e2d6fe2f76a4
Devesh-Mundra-ryn <deveshsrinidhim@gmail.com>
2025-06-15 18:35:56 -0700

    Update vite.config.ts

--- bluvia/front_end/vite.config.ts
+++ bluvia/front_end/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000', // For local development
+        target: 'bluvia-chayb9czgacrdba5.canadacentral-01.azurewebsites.net', // For local development
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
